379cbf83d11465e81073a32381c55c412931e197
Anton Glazunov <kampel2012@yandex.ru>
2023-07-24 15:51:20 +0500

    fix test

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -12,29 +12,29 @@ const requestNotificationPermission = () => {
 
 // Функция для создания уведомления
 const createNotification = (reminderText) => {
+  new Notification('Напоминание', {
+    body: reminderText,
+  });
+};
+
+// Функция для планирования показа уведомления через определенное время
+const scheduleNotification = (reminderText, reminderTime) => {
+  requestNotificationPermission();
   if (!('Notification' in window)) {
     console.log('This browser does not support notifications.');
     return;
   }
-
   if (
     Notification.permission === 'granted' ||
     Notification.permission === 'default'
   ) {
     requestNotificationPermission();
-    new Notification('Напоминание', {
-      body: reminderText,
-    });
+    const reminderTimeMilliseconds = parseInt(reminderTime); // Переводим время в миллисекунды (1 час = 3600000 миллисекунд / минутка 60000 мс)
+    alert('Уведомление успешно создано ');
+    setTimeout(() => {
+      createNotification(reminderText);
+    }, reminderTimeMilliseconds);
   }
-};
-
-// Функция для планирования показа уведомления через определенное время
-const scheduleNotification = (reminderText, reminderTime) => {
-  const reminderTimeMilliseconds = parseInt(reminderTime); // Переводим время в миллисекунды (1 час = 3600000 миллисекунд / минутка 60000 мс)
-  alert('Уведомление успешно создано ');
-  setTimeout(() => {
-    createNotification(reminderText);
-  }, reminderTimeMilliseconds);
 };
 
 // * Итоговая функция
