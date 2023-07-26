1879cf01cb6c1932505257071615833ae940ff2f
Polina Andreyanova <andreyanovalina@yandex.ru>
2023-07-26 17:21:13 +0300

    перевести Promo и Signin

--- frontend/src/utils/dictionary.js
+++ frontend/src/utils/dictionary.js
@@ -12,5 +12,41 @@ export function getDictionary() {
       RU: 'Скачать',
       EN: 'Download',
     },
+    promoTitle: {
+      RU: 'Превращаем аудиозаписи в текст с высокой точностью и оптимизацией',
+      EN: 'We turn audio recordings into text with high accuracy and optimization',
+    },
+    promoSubtitle: {
+      RU: 'Помогаем пользователям сокращать, улучшать, трансформировать аудиозаписи и экономить время',
+      EN: 'We help users to reduce, improve, transform audio recordings and save time',
+    },
+    promoBtn: {
+      RU: 'Начать использование',
+      EN: 'Start using',
+    },
+    loginTitle: {
+      RU: 'Вход',
+      EN: 'Entrance',
+    },
+    emailInputTitle: {
+      RU: 'Введите ваш e-mail',
+      EN: 'Enter your e-mail',
+    },
+    passwordInputTitle: {
+      RU: 'Введите пароль',
+      EN: 'Enter the password',
+    },
+    enterBtn: {
+      RU: 'Войти',
+      EN: 'Enter',
+    },
+    noAccountTxt: {
+      RU: 'Нет аккаунта?',
+      EN: 'No account?',
+    },
+    registerBtn: {
+      RU: 'Зарегистрироваться',
+      EN: 'Register',
+    }
   };
 }
--- frontend/src/views/PromoPage.jsx
+++ frontend/src/views/PromoPage.jsx
@@ -1,14 +1,14 @@
 import micIcon from '../assets/icon/mic.svg';
 import vector from '../assets/VectorPromo.svg';
 import MyButton from '../components/UI/MyButton';
-/* import { useContext } from 'react';
+import { useContext } from 'react';
 import { LanguageContext } from '../context/LanguageContext';
-import { getDictionary } from '../utils/dictionary'; */
+import { getDictionary } from '../utils/dictionary';
 import Header from '../components/Header';
 
 const PromoPage = () => {
-  /*   const { language } = useContext(LanguageContext);
-  const dictionary = getDictionary(); */
+  const { language } = useContext(LanguageContext);
+  const dictionary = getDictionary();
 
   return (
     <>
@@ -16,15 +16,14 @@ const PromoPage = () => {
       <div className="bg-bgFrame relative z-0">
         <div className="container min-h-[calc(100vh-80px)] text-center max-w-6xl">
           <h1 className="text-6xl font-extrabold leading-tight max-w-6xl mx-auto text-primary mb-4 pt-52">
-            Превращаем аудиозаписи в текст с высокой точностью и оптимизацией
+            {dictionary.promoTitle[language]}
           </h1>
           <p className="text-secondary text-2xl max-w-2xl mx-auto mb-10 font-medium">
-            Помогаем пользователям сокращать, улучшать, трансформировать
-            аудиозаписи и экономить время
+            {dictionary.promoSubtitle[language]}
           </p>
           <div className="mx-auto max-w-max pb-20">
             <MyButton>
-              Начать использование <img src={micIcon} alt="Микрофон" />
+              {dictionary.promoBtn[language]} <img src={micIcon} alt="Микрофон" />
             </MyButton>
           </div>
         </div>
