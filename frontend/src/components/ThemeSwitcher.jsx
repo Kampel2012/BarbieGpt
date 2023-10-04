c14095ec8f565628f052150186e2ffe056658b50
Anton Glazunov <kampel2012@yandex.ru>
2023-10-04 15:34:33 +0500

    Feature написать свитчер темы

--- frontend/src/components/ThemeSwitcher.jsx
+++ frontend/src/components/ThemeSwitcher.jsx
@@ -1,12 +1,8 @@
-import { useState /* useContext */ } from 'react';
-// import { LanguageContext } from '../context/LanguageContext';
+import { useState } from 'react';
 import sunIcon from '../assets/icon/sun.svg';
 import moonIcon from '../assets/icon/moon.svg';
 
-// TODO сделать темную тему после хакатона
-
 const ThemeSwitcher = () => {
-  /*   const { language, setLanguage } = useContext(LanguageContext); */
   const [language, setLanguage] = useState('RU');
   const handleLanguageChange = (language) => {
     setLanguage(language);
@@ -18,16 +14,14 @@ const ThemeSwitcher = () => {
         className={`px-3 py-2 rounded-xl flex gap-2 transition-all leading-5 ${
           language === 'EN' ? 'bg-gray-200' : 'bg-inherit'
         }`}
-        onClick={() => handleLanguageChange('EN')}
-      >
+        onClick={() => handleLanguageChange('EN')}>
         <img src={sunIcon} alt="Переключение на светлую тему" />
       </button>
       <button
         className={`px-3 py-2 rounded-xl flex gap-2 transition-all leading-5 ${
           language === 'RU' ? 'bg-gray-200' : 'bg-inherit'
         }`}
-        onClick={() => handleLanguageChange('RU')}
-      >
+        onClick={() => handleLanguageChange('RU')}>
         <img src={moonIcon} alt="Переключение на темную тему" />
       </button>
     </div>
